22d7f96bcb7a20c1df2ff83b48adfe9b1cfc0365
Nenza Nurfirmansyah <nenza.dev@proton.me>
2026-04-02 11:44:29 +0700

    change style

---
+++ script.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card, index) => {
+  card.style.opacity = 0;
+  card.style.transform = "translateY(20px)";
+
+  setTimeout(() => {
+    card.style.transition = "0.5s";
+    card.style.opacity = 1;
+    card.style.transform = "translateY(0)";
+  }, index * 200);
+});
